02a34cf6e49a499229809ffba744ec2c8ef043c6
Alejandro Lara <alelaru@gmail.com>
2021-06-04 01:47:03 +0200

    We configured firebase and stucked in  Router

---
+++ src/context/firebase.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+// Initialize Firebase
+const FirebaseContext = createContext(null);
+
+export default FirebaseContext;
---
+++ src/lib/firebase.js
@@ -0,0 +1,24 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+//Here i want to import the seed file
+import { seedDatabase } from "../seed"
+
+const config = { 
+    apiKey: "AIzaSyAqnSO_IQ-iPg2ZSxlgkaC87sca2RE8-Ik",
+    authDomain: "instagramclone-c2164.firebaseapp.com",
+    projectId: "instagramclone-c2164",
+    storageBucket: "instagramclone-c2164.appspot.com",
+    messagingSenderId: "71570511270",
+    appId: "1:71570511270:web:e6517ac35d9188c9e8ab67"
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue }  = Firebase.firestore;
+
+//Here is where i want to call the config once
+//We did it once and we are done
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
