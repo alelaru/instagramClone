de59edcaf08777c2624fd485c66cb50bea2bc387
Alejandro Lara <alelaru@gmail.com>
2021-06-07 16:00:18 +0200

    Front end loginpage done

--- src/pages/login.js
+++ src/pages/login.js
@@ -13,7 +13,10 @@ const Login = () => {
     const isInvalid = password === "" || emailAddress === "";
 
 
-    const handleLogin = () => {};
+    const handleLogin = (e) => {
+        e.preventDefault();
+        
+    };
 
     useEffect(() => {
         document.title = "Login - Instagram"
@@ -25,10 +28,52 @@ const Login = () => {
                 <img src="/images/iphone-with-profile.jpg" alt="iPhone with Instagram app"/>
             </div>
             <div className="flex flex-col w-2/5">
-               <p>i will be the login form</p> 
+                <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4">
+               <h1 className="flex justify-center w-full">
+                    <img src="/images/logo.png" alt="instagram" className="mt-2 w6-/12 mb-4"></img>
+               </h1>
+            { error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+            <form onSubmit={handleLogin} method="POST">
+                <input aria-label="Enter your email address"
+                    type="text"
+                    placeholder="Email address"
+                    className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                    onChange={(e) => setEmailAddress(e.target.value)}
+                ></input>
+                <input aria-label="Enter your password"
+                    type="password"
+                    placeholder="Password"
+                    className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+                    onChange={(e) => setPassword(e.target.value)}
+                ></input>
+                <button
+                    disabled={isInvalid}
+                    type="submit"
+                    className={
+                        `bg-blue-500 text-white w-full rounded h-8 font-bold
+                        ${isInvalid && 'opacity-50'}`
+                    }
+                    onSubmit={handleLogin}>
+                        Log In
+                </button>                
+            </form>
             </div>
+
+            <div className="flex justify-center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+                <p className="text-sm">
+                    Don't have an account?
+                </p>
+            </div>
+            </div>
+
         </div>
         );
 }
 
 export default Login;
+
+//bg-blue-medium to do at the tailwind config
+//text-gray-base -> hex values
+//text-red-primary -> hex values
+//border-gray-primary -> hex values
