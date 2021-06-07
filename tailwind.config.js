451e0df3350704bfdc4f08e00a86983ad8bf890b
Alejandro Lara <alelaru@gmail.com>
2021-06-07 21:12:35 +0200

    Sign In Page done, lets work on the sign in functionality

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,36 @@
+//bg-blue-medium to do at the tailwind config
+//text-blue-medium -> hex value
+//text-gray-base -> hex values
+//text-red-primary -> hex values
+//border-gray-primary -> hex values
+
+
 module.exports = {
     future: {
       removeDeprecatedGapUtilities: true
+    },
+    theme: {
+        fill: (theme) => ({
+            red: theme('colors.red.primary')
+          }),
+        colors: {
+            white: "#ffffff",
+            blue: {
+                medium: "#005c98",
+
+            },
+            black: {
+                light: "#005c98",
+                faded: "#00000059"
+            },
+            gray: {
+                base: "616161",
+                background: "#fafafa",
+                primary: "#dbdbdb"
+            },
+            red: {
+                primary: "ed4956"
+            }
+        }
     }
 }
