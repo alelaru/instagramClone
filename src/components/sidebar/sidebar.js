c8651376a3dc7ff7194672adf5107cf8cface172
Alejandro Lara <alelaru@gmail.com>
2021-06-13 21:40:47 +0200

    We added the user with the image, username and fullName to the Sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -6,7 +6,6 @@ import User from "./user";
 const Sidebar = () => {
 
     const {user: {username, fullName, userId}}  = useUser();
-    console.log(username);
 
     return ( 
     <div className="p-4">
--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -8,8 +8,18 @@ const User = ({username, fullName}) => {
         <Skeleton count={1} height={61}></Skeleton>
         )
     : ( 
-        <Link to={`/p/${username}`} className="grid">
-            <p>{username}</p>
+        <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+            <div className="flex items-center justify-between col-span-1">
+                <img 
+                    className="rounded-full w-16 flex mr-3"
+                    src={`/images/avatars/${username}.jpg`} 
+                    alt="">    
+                </img>
+            </div>
+            <div className="col-span-3">
+                <p className="font-bold text-sm">{username}</p>
+                <p className="text-sm">{fullName}</p>
+            </div>
         </Link>
     )
 }   
@@ -17,7 +27,7 @@ const User = ({username, fullName}) => {
 export default User;
 
 User.propTypes = {
-    username: PropTypes.string,
-    fullName: PropTypes.string
+    username: PropTypes.string.isRequired,
+    fullName: PropTypes.string.isRequired
 
 }
