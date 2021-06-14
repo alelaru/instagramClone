6b409e0474f93a1513e449a929692da08571daff
Alejandro Lara <alelaru@gmail.com>
2021-06-14 13:40:36 +0200

    Suggested profiles avatar pictures done

--- src/components/sidebar/suggested-profile.js
+++ src/components/sidebar/suggested-profile.js
@@ -1,5 +1,30 @@
-const SuggestedProfile = () => {
-    return ( <p>Suggested Profile</p>);
+import PropTypes from "prop-types";
+import { useState } from "react";
+
+
+const SuggestedProfile = ({userDocId, username, profileId, userId}) => {
+    const [followed, setFollowed] = useState(false)
+
+    return !followed 
+    ? ( 
+        <div className="flex flex-row items-center align-items justify-between">
+            <div className="flex items-center justify-between">
+                <img
+                    className="rounded-full w-8 flex m-3"
+                    src={`/images/avatars/${username}.jpg`}
+                    alt=""
+                ></img>
+            </div>
+        </div>
+    )
+    : null
 }
  
 export default SuggestedProfile;
+
+SuggestedProfile.propTypes = {
+    userId : PropTypes.string.isRequired,
+    userDocId: PropTypes.string.isRequired,
+    profileId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired
+}
